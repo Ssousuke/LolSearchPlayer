da0171bc0690609744e812f68d876c131ae6a17c
Wesley Farias <wesleysousukee@gmail.com>
2023-01-28 20:34:58 -0300

    add project api

---
+++ LolSearchPlayer.Api/Controllers/SummonerController.cs
@@ -0,0 +1,26 @@
+﻿using LolSearchPlayer.Dto.Dto;
+using LolSearchPlayer.Infraestructure.Interfaces;
+using LolSearchPlayer.Infraestructure.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LolSearchPlayer.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SummonerController : ControllerBase
+    {
+        private ISumonnerServices _services;
+        public SummonerController(ISumonnerServices services)
+        {
+            _services = services;
+        }
+
+        [HttpGet("buscar/{name}")]
+        public async Task<ActionResult<SummonerDto>> Get(string name)
+        {
+            var summoner = await _services.GetSummoner(name);
+            return summoner;
+        }
+    }
+}
